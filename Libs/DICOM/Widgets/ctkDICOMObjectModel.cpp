01d8f9c4a176fdb8b44918adca7c8480d68b5b2d
Mouhamed DIOP <mouhamed.diop@esp.sn>
2023-02-23 10:14:13 +0000

    BUG: Mark translatable strings in DICOM/Widgets
    see Slicer/SlicerLanguagePacks#17
    see Slicer/SlicerLanguagePacks#12

--- Libs/DICOM/Widgets/ctkDICOMObjectModel.cpp
+++ Libs/DICOM/Widgets/ctkDICOMObjectModel.cpp
@@ -78,11 +78,11 @@ void ctkDICOMObjectModelPrivate::init()
 {
   Q_Q(ctkDICOMObjectModel);
   QStringList horizontalHeaderLabels;
-  horizontalHeaderLabels.append( QString("Tag"));
-  horizontalHeaderLabels.append( QString("Attribute"));
-  horizontalHeaderLabels.append( QString("Value"));
-  horizontalHeaderLabels.append( QString("VR"));
-  horizontalHeaderLabels.append( QString("Length"));
+  horizontalHeaderLabels.append(ctkDICOMObjectModel::tr("Tag"));
+  horizontalHeaderLabels.append(ctkDICOMObjectModel::tr("Attribute"));
+  horizontalHeaderLabels.append(ctkDICOMObjectModel::tr("Value"));
+  horizontalHeaderLabels.append(ctkDICOMObjectModel::tr("VR"));
+  horizontalHeaderLabels.append(ctkDICOMObjectModel::tr("Length"));
   q->setHorizontalHeaderLabels(horizontalHeaderLabels);
 }
 
@@ -114,7 +114,7 @@ void ctkDICOMObjectModelPrivate::itemInsert(ctkDICOMItem* dicomItem, DcmItem *da
     DcmTag tagKey = tag.getXTag();
     if( tagKey == DCM_SequenceDelimitationItem
         || tagKey == DCM_ItemDelimitationItem
-        || "Item" == tagName)
+        || /*no tr*/"Item" == tagName)
       {
       return;
       }
